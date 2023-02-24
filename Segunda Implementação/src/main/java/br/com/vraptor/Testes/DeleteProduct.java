40e2c5cdf2e6106217ec35b08dd995012780828c
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-02-24 16:13:43 -0300

    Delete Segunda Implementação directory

--- Segunda Implementação/src/main/java/br/com/vraptor/Testes/DeleteProduct.java
+++
@@ -1,9 +0,0 @@
-package br.com.vraptor.Testes;
-
-import br.com.vraptor.Infra.ProductDAO;
-
-public class DeleteProduct {
-	public static void main(String[] args) {
-		new ProductDAO().delete(1L);
-	}
-}
--- Segunda Implementação/src/main/java/br/com/vraptor/Testes/GetUnique.java
+++
@@ -1,9 +0,0 @@
-package br.com.vraptor.Testes;
-
-import br.com.vraptor.Infra.ProductDAO;
-
-public class GetUnique {
-	public static void main(String[] args) {
-		System.out.println(new ProductDAO().get(2L));
-	}
-}
