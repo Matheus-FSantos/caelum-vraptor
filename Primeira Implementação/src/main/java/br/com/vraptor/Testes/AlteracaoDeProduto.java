3da1367bd95886ec4d200a1c625c78a64f96ba93
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-02-24 16:13:35 -0300

    Delete Primeira Implementação directory

--- Primeira Implementação/src/main/java/br/com/vraptor/Testes/AlteracaoDeProduto.java
+++
@@ -1,9 +0,0 @@
-package br.com.vraptor.Testes;
-
-import br.com.vraptor.Infra.ProdutoDAO;
-
-public class AlteracaoDeProduto {
-	public static void main(String[] args) {
-		new ProdutoDAO().edit(5L, "Mesa", "Mesa de jantar", 100.00);
-	}
-}
--- Primeira Implementação/src/main/java/br/com/vraptor/Testes/RemocaoDeProduto.java
+++
@@ -1,9 +0,0 @@
-package br.com.vraptor.Testes;
-
-import br.com.vraptor.Infra.ProdutoDAO;
-
-public class RemocaoDeProduto {
-	public static void main(String[] args) {
-		new ProdutoDAO().delete(6L);
-	}
-}
